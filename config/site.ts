ae929efc09bc89309746c9c12b668908a97180c6
Batikann <emiruar123@gmail.com>
2024-03-13 20:25:12 +0300

    stage-3:Site-Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'jollyBlog',
+  url: 'https://example.com',
+  description: 'Nextjs 14 blog using velite,tailwind and shadcn',
+  author: 'batik',
+  links: {
+    twitter: '',
+    github: '',
+    personalSite: '',
+  },
+}
+
+export type siteConfig = typeof siteConfig
